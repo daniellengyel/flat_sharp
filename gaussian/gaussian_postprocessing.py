f2e0d9b6525c96cc3be5d84e54bbe0da28a9ccfd
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2020-03-25 21:11:47 +0100

    cleaned up the code so we can run on cluster

---
+++ gaussian/gaussian_postprocessing.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+
+from utils import *
+from gaussian_training import *
+
+import yaml, os, sys
+
+# get hessians...
+import torch
+from hessian_eigenthings import compute_hessian_eigenthings
+
+import pickle
+# +++ process experiment results +++
+
+def temp_tb_to_dict(path_to_events_file):
+    nets_dict_potential = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
+    nets_dict_acc = defaultdict(lambda: defaultdict(float))
+
+    for e in summary_iterator(path_to_events_file):
+        for v in e.summary.value:
+            if ("Potential" in v.tag):
+                net_num = int(v.tag.split("_")[1])  # Todo make this /
+                name = v.tag.split("/")[1]
+
+                if e.step not in nets_dict_potential[net_num]:
+                    nets_dict_potential[net_num][e.step] = {}
+                nets_dict_potential[net_num][e.step][name] = v.simple_value
+            elif "Accuracy" in v.tag:
+                net_num = int(v.tag.split("_")[1])  # Todo make this /
+
+                nets_dict_acc[net_num][e.step] = v.simple_value
+
+    return nets_dict_potential, nets_dict_acc
+
+def get_models(model_folder_path, step):
+    nets_dict = {}
+    for root, dirs, files in os.walk(model_folder_path):
+        for name in files:
+            name_split_underscore = name.split("_")
+            if name_split_underscore[-1].split(".")[0] == str(step):
+                file_path = os.path.join(root, name)
+                with open(file_path, "rb") as f:
+                    net = torch.load(f)
+                nets_dict[name_split_underscore[1]] = net
+    return nets_dict
+
+def get_last_acc_potential(nets_dict_potential, nets_dict_acc, potential_type="total"):
+    w = []
+    a = []
+    for nn in range(len(nets_dict_acc)):
+        last_step = int(list(nets_dict_acc[nn].keys())[-2])
+        a.append(nets_dict_acc[nn][last_step])
+        w.append(nets_dict_potential[nn][last_step][potential_type])
+    return w, a, last_step
+
+def get_last(run_file_path):
+    potential, acc = temp_tb_to_dict(run_file_path)
+    return get_last_acc_potential(potential, acc)
+
+# iterate through runs
+def get_runs(experiment_folder):
+    run_config_dir = {}
+    run_acc_pot_dir = {}
+    for root, dirs, files in os.walk("{}/runs".format(experiment_folder), topdown=False):
+        if len(files) != 2:
+            continue
+        run_file_name, config_name = files
+        curr_dir = os.path.basename(root)
+
+        # get acc-potentials
+        try:
+            with open(os.path.join(root, config_name), "r") as f:
+                config = yaml.safe_load(f)
+            acc_potentials_step = get_last(os.path.join(root, run_file_name))
+            run_config_dir[curr_dir] = config
+            run_acc_pot_dir[curr_dir] = acc_potentials_step
+        except:
+            print(root)
+
+    return run_acc_pot_dir, run_config_dir
+
+# get eigenvalues of final model.
+def get_eig(experiment_folder, step, use_gpu=False):
+    # init
+    eigenvalue_dict = {}
+    num_eigenthings = 5  # compute top 5 eigenvalues/eigenvectors
+    loss = torch.nn.CrossEntropyLoss()
+    with open(os.path.join(experiment_folder, "data.pkl"), "rb") as f:
+        data = pickle.load(f)
+
+    dataloader = DataLoader(data[0], batch_size=16, shuffle=True)  # fix the batch size
+
+    # iterate through models
+    for root, dirs, files in os.walk("{}/models".format(experiment_folder), topdown=False):
+        if len(files) == 0:
+            continue
+
+        try:
+            curr_dir = os.path.basename(root)
+            eigenvalue_dict[curr_dir] = {}
+            models_dict = get_models(root, step)
+            # get eigenvals
+            for k, m in models_dict.items():
+                eigenvals, eigenvecs = compute_hessian_eigenthings(m, dataloader,
+                                                                   loss, num_eigenthings, use_gpu=use_gpu)
+                eigenvalue_dict[curr_dir][k] = (eigenvals, eigenvecs)
+        except:
+            print(root)
+
+    return eigenvalue_dict
+
+
+if __name__ == "__main__":
+    # get test--accuracy and total weight.
+    experiment_name = "Mar22_17-16-50_Daniels-MacBook-Pro-4.local"
+    experiment_folder = "./gaussian_experiments/{}".format(experiment_name)
+
+
---
+++ gaussian/gaussian_training.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy, yaml
+import torch
+import torchvision
+import torch.optim as optim
+
+from torch.utils.tensorboard import SummaryWriter
+
+from utils import *
+from nets.Nets import SimpleNet
+
+from torch.utils.data import DataLoader
+from dataloaders import GaussianMixture
+
+import os, time
+
+
+def train(data, config, folder_path):
+    # init torch
+    # device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+    torch.backends.cudnn.enabled = False
+    if config["torch_random_seed"] is not None:
+        torch.manual_seed(config["torch_random_seed"])
+
+    # get data
+    train_loader = DataLoader(data[0], batch_size=config["batch_train_size"], shuffle=True)
+    test_loader = DataLoader(data[1], batch_size=config["batch_test_size"], shuffle=True)
+
+
+    # Init neural nets and weights
+    num_nets = config["num_nets"]
+    net_params = config["net_params"]
+    nets = [SimpleNet(*net_params) for _ in range(num_nets)]
+    nets_weights = np.zeros(num_nets)
+
+    #  Define a Loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizers = [optim.SGD(nets[i].parameters(), lr=config["learning_rate"],
+                            momentum=config["momentum"]) for i in range(num_nets)]
+
+    # Set algorithm params
+    weight_type = config["weight_type"]
+
+    beta = config["softmax_beta"]
+
+    # init saving
+    file_stamp = time.time() #get_file_stamp()
+    writer = SummaryWriter("{}/runs/{}".format(folder_path, file_stamp))
+    os.makedirs("{}/models/{}".format(folder_path, file_stamp))
+    with open("{}/runs/{}/{}".format(folder_path, file_stamp, "config.yml"), "w") as f:
+        yaml.dump(config, f, default_flow_style=False)
+
+    # train
+    for epoch in range(config["num_epochs"]):
+        # get train loaders for each net
+        net_data_loaders = [iter(enumerate(train_loader, 0)) for _ in range(num_nets)]
+
+        is_training_epoch = True
+        while is_training_epoch:
+            # TODO make inner loop a function. That way we can have different methods of looping over data.
+            for idx_net in range(num_nets):
+                # get net and optimizer
+                net = nets[idx_net]
+                optimizer = optimizers[idx_net]
+
+                # get the inputs; data is a list of [inputs, labels]
+                try:
+                    i, data = next(net_data_loaders[idx_net])
+                except:
+                    is_training_epoch = False
+                    break
+                inputs, labels = data
+
+                # Compute gradients for input.
+                inputs.requires_grad = True
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = net(inputs)
+                loss = criterion(outputs.float(), labels)
+                loss.backward(retain_graph=True)
+                optimizer.step()
+
+                # update weights
+                if weight_type == "input_output_forbenius":
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # get input gradients
+                    output_forb = torch.norm(outputs)
+                    output_forb.backward()
+                    input_grads = inputs.grad
+
+                    curr_weight = weight_function_input_jacobian(input_grads)
+                    nets_weights[idx_net] += curr_weight
+                elif weight_type == "loss_gradient_weights":
+
+                    param_grads = get_grad_params_vec(net)
+                    curr_weight = np.linalg.norm(param_grads)
+                    nets_weights[idx_net] += curr_weight
+                else:
+                    raise NotImplementedError()
+
+                # store training accuracy current
+                writer.add_scalar('Loss/train/net_{}'.format(idx_net), loss, i + epoch*len(train_loader))
+
+                writer.add_scalar('Potential/curr/net_{}'.format(idx_net), curr_weight, i + epoch*len(train_loader))
+                writer.add_scalar('Potential/total/net_{}'.format(idx_net), nets_weights[idx_net], i + epoch*len(train_loader))
+
+
+            writer.add_scalar('Kish/', kish_effs(nets_weights), i + epoch*len(train_loader))
+
+            # Get variation of network weights
+            writer.add_scalar('WeightVarTrace/', np.trace(get_params_var(nets)), i + epoch*len(train_loader))
+
+
+            # Check resample
+            if kish_effs(nets_weights) < config["ess_threshold"]:
+                # resample particles
+                sampled_idx = sample_index_softmax(nets_weights, nets, beta=beta)
+                # init nets etc
+                nets = [copy.deepcopy(nets[i]) for i in sampled_idx]
+                optimizers = [optim.SGD(nets[i].parameters(), lr=config["learning_rate"],
+                                        momentum=config["momentum"]) for i in range(num_nets)]
+                nets_weights = np.zeros(num_nets)
+                # TODO save which models got swapped how
+
+        # get test error
+        for idx_net in range(num_nets):
+            correct = 0
+            _sum = 0
+
+            for idx, (test_x, test_label) in enumerate(test_loader):
+                predict_y = nets[idx_net](test_x.float()).detach()
+                predict_ys = np.argmax(predict_y, axis=-1)
+                label_np = test_label.numpy()
+                _ = predict_ys == test_label
+                correct += np.sum(_.numpy(), axis=-1)
+                _sum += _.shape[0]
+
+                writer.add_scalar('Accuracy/net_{}'.format(idx_net), correct / _sum, epoch*len(train_loader) + i)
+
+        for idx_net in range(num_nets):
+            torch.save(nets[idx_net], '{}/models/{}/net_{}_step_{}.pkl'.format(folder_path, file_stamp, idx_net, epoch*len(train_loader) + i))
+
+
+
+    return nets
+
+
+
